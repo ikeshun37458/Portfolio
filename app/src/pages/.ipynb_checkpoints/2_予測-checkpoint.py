373cdaca70735452759b53b2bf5230e5b205e8e3
ikeshun37458 <ikeshun1130@icloud.com>
2024-12-22 14:30:57 +0900

    Update データアップロード.py

---
+++ app/src/pages/.ipynb_checkpoints/2_予測-checkpoint.py
@@ -0,0 +1,76 @@
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+import requests
+import pandas as pd
+import streamlit as st
+
+st.title("予測")
+
+if "df1" not in st.session_state:
+    st.session_state.df1 = None
+
+if st.session_state.df1 is None:
+    st.write("データがアップロードされていません。データアップロードに戻りアップロードしてください！")
+else:
+    df_all_cate = st.session_state.df1
+    cate_list = df_all_cate["category"].unique()
+    target_cate = st.selectbox(
+        "どのカテゴリを確認しますか？",
+        cate_list,
+        index=None,
+        placeholder="選択してください"
+    )
+
+    if target_cate != None:
+        # データ準備
+        data = df_all_cate[df_all_cate["category"].str.contains(target_cate)].iloc[-12:].copy()
+        expen_cate = data["expen_cate"].tolist()
+
+        model_status = st.empty()
+        model_status.write("モデルを学習中...")
+        # サーバーにデータを送信
+        response = requests.post("http://127.0.0.1:8000/train/", json={"data": expen_cate, "step": 3})
+    
+        if response.status_code == 200:
+            model_status.empty()
+            prediction = response.json()["prediction"]
+            st.write(f"予測された来月の支出額：¥{prediction:,.0f}")
+
+            # 直近1年の年月リストを取得
+            date = data["year-month"].tolist()
+            last_month = date[-1]
+            year, month = int(last_month[:4]), int(last_month[5:7])
+
+            # 来月の年月を取得
+            if month == 12:
+                month = 1
+                year += 1
+            else:
+                month += 1
+            next_month = f"{year}-{month:02d}"
+
+            plt.plot(
+                date,
+                expen_cate,
+                marker=".",
+                color="lightseagreen",
+                linewidth=2
+            )
+            plt.plot(
+                [date[-1], next_month],
+                [expen_cate[-1], prediction],
+                marker=".",
+                linestyle="--",
+                color="yellow",
+                linewidth=2
+            )
+            
+            plt.xticks(rotation=45, fontsize=10)
+            plt.xlabel("支出の月別推移", fontsize=10)
+            plt.ylabel("支出", rotation=0, fontsize=10, labelpad=15)
+            plt.tight_layout()
+            plt.style.use("dark_background")
+            st.pyplot(plt)
+        else:
+            model_status.empty()
+            st.write("データ数が十分にありません。他のカテゴリを選択してください！")
--- app/src/データアップロード.py
+++ app/src/データアップロード.py
@@ -10,8 +10,6 @@ uploaded_file = uploader.file_uploader("Zaimの入力データをアップロー
 # 初期化
 if "df1" not in st.session_state:
     st.session_state.df1 = None 
-if "df2" not in st.session_state:
-    st.session_state.df2 = None 
 
 if uploaded_file:
     uploader.empty()
@@ -19,9 +17,8 @@ if uploaded_file:
     df_org = pd.read_csv(uploaded_file, encoding="utf-8")
     st.success("データがアップロードされました！")
 
+    # データの前処理
     df_org["日付"] = df_org["日付"].apply(lambda x: x[:7])
-    df_org[["year", "month"]] = df_org["日付"].str.split("-", expand=True)
-    
     df_org = df_org.rename(columns={
         "日付": "year-month",
         "カテゴリ": "category",
@@ -34,7 +31,6 @@ if uploaded_file:
         "sub_category",
         "expenses"
     ]]
-    
     df_org = df_org[df_org["expenses"] > 0]
 
     # 年月のリスト
@@ -54,23 +50,7 @@ if uploaded_file:
         df_all_cate = pd.concat([df_all_cate, df_total_cate], ignore_index=True)
     
     st.session_state.df1 = df_all_cate
-
-    # 詳細カテゴリのDataFrameを作成→保持
-    df_all_sub_cate = pd.DataFrame()
-    for date in date_list:
-        df_filtered = df_org[df_org["year-month"] == date]
-        df_total_sub_cate = df_filtered.copy()
-        
-        df_total_sub_cate["expen_sub_cate"] = df_filtered.groupby("sub_category")["expenses"].transform("sum")
-        df_total_sub_cate = df_total_sub_cate.drop_duplicates(subset=["sub_category"])
-        
-        df_total_sub_cate = df_total_sub_cate.sort_values(by=["category"])
-        df_total_sub_cate = df_total_sub_cate.drop(columns=["expenses"])
-        df_all_sub_cate = pd.concat([df_all_sub_cate, df_total_sub_cate], ignore_index=True)
-
-    st.session_state.df2 = df_all_sub_cate
     
-    
-if (st.session_state.df1 is not None) and (st.session_state.df2 is not None):
+if st.session_state.df1 is not None:
     uploader.empty()
     st.write("データがアップロードされています。\n実績または予測にお進みください！")
